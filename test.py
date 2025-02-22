78cdd4fe716beeff084c6d25269333b4d6a8b593
patnitu <patnitu@gmail.com>
2025-02-22 11:41:23 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 def fetch_btc_data():
     url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart"
-    params = {"vs_currency": "usd", "days": "1", "interval": "minute"}
+    params = {"vs_currency": "usd", "days": "1", "interval": "1"}
     
     response = requests.get(url, params=params)
     data = response.json()
